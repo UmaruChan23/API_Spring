5dca384d5dd43561207a00d7be803d606932d7f4
UMRchan <f.matorin@mail.ru>
2021-02-24 17:27:49 +0300

    Created method for change todo title

--- src/main/java/com/example/Test/controller/TodoController.java
+++ src/main/java/com/example/Test/controller/TodoController.java
@@ -2,9 +2,11 @@ package com.example.Test.controller;
 
 import com.example.Test.entity.TodoEntity;
 import com.example.Test.exeption.PermissionDeniedException;
+import com.example.Test.security.AppUserDetails;
 import com.example.Test.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,40 +17,69 @@ public class TodoController {
     @Autowired
     private TodoService todoService;
 
+    private AppUserDetails userPrincipal;
+
     @PostMapping
     public ResponseEntity createTodo(@RequestBody TodoEntity todo,
-                                     @RequestParam Long userId) {
-        try {
-            return ResponseEntity.ok(todoService.createTodo(todo, userId));
-        } catch (PermissionDeniedException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Ошибочка вышла :(");
-
+                                     @RequestParam Long userId, Authentication authentication) {
+        if (checkUser(userId, authentication)) {
+            try {
+                return ResponseEntity.ok(todoService.createTodo(todo, userId));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Ошибочка вышла :(");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("В доступе отказано");
         }
+    }
 
+    @PatchMapping
+    public ResponseEntity changeTodo(@RequestBody String title,
+                                     @RequestParam Long todoId,
+                                     @RequestParam Long userId, Authentication authentication) {
+        if (checkUser(userId, authentication)) {
+            try {
+                return ResponseEntity.ok(todoService.changeTodo(title, todoId));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Ошибочка вышла :(");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("В доступе отказано");
+        }
     }
 
     @PutMapping
-    public ResponseEntity completeTodo(@RequestParam Long id) {
-        try {
-            return ResponseEntity.ok(todoService.completeTodo(id));
-        } catch (PermissionDeniedException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Ошибочка вышла :(");
-
+    public ResponseEntity completeTodo(@RequestParam Long todoId,
+                                       @RequestParam Long userId, Authentication authentication) {
+        if (checkUser(userId, authentication)) {
+            try {
+                return ResponseEntity.ok(todoService.completeTodo(todoId));
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("Ошибочка вышла :(");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("В доступе отказано");
         }
     }
 
     @DeleteMapping
-    public ResponseEntity deleteTodo(@RequestParam Long id) {
-        try {
-            return ResponseEntity.ok(todoService.deleteTodo(id));
-        } catch (PermissionDeniedException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("ERROR");
+    public ResponseEntity deleteTodo(@RequestParam Long todoId,
+                                     @RequestParam Long userId, Authentication authentication) {
+        if (checkUser(userId, authentication)) {
+            try {
+                return ResponseEntity.ok(todoService.deleteTodo(todoId));
+            } catch (PermissionDeniedException e) {
+                return ResponseEntity.badRequest().body(e.getMessage());
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("ERROR");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("В доступе отказано");
         }
+    }
+
+    public boolean checkUser(Long userId, Authentication authentication) {
+        userPrincipal = (AppUserDetails) authentication.getPrincipal();
+        return userPrincipal.getId().equals(userId) || userPrincipal.getRole().equals("ROLE_ADMIN");
     }
 }
--- src/main/java/com/example/Test/service/TodoService.java
+++ src/main/java/com/example/Test/service/TodoService.java
@@ -7,13 +7,7 @@ import com.example.Test.model.Todo;
 import com.example.Test.repository.TodoRepo;
 import com.example.Test.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -24,41 +18,25 @@ public class TodoService {
     private UserRepo userRepo;
 
     public Todo createTodo(TodoEntity todo, Long userId) throws PermissionDeniedException {
-        if (inspectUserById(userId)) {
-            UserEntity user = userRepo.findById(userId).get();
-            todo.setUser(user);
-            return Todo.toModel(todoRepo.save(todo));
-        } else {
-            throw new PermissionDeniedException("В доступе отказано");
-        }
+        UserEntity user = userRepo.findById(userId).get();
+        todo.setUser(user);
+        return Todo.toModel(todoRepo.save(todo));
+    }
 
+    public Todo changeTodo(String todoText, Long todoId){
+        TodoEntity todo = todoRepo.findById(todoId).get();
+        todo.setTitle(todoText);
+        return Todo.toModel(todoRepo.save(todo));
     }
 
     public Todo completeTodo(Long id) throws PermissionDeniedException {
-        if (inspectUserById(id)) {
-            TodoEntity todo = todoRepo.findById(id).get();
-            todo.setCompleted(!todo.getCompleted());
-            return Todo.toModel(todoRepo.save(todo));
-        } else {
-            throw new PermissionDeniedException("В доступе отказано");
-        }
-
+        TodoEntity todo = todoRepo.findById(id).get();
+        todo.setCompleted(!todo.getCompleted());
+        return Todo.toModel(todoRepo.save(todo));
     }
 
     public Long deleteTodo(Long id) throws PermissionDeniedException {
-        if (inspectUserById(id)) {
-            todoRepo.deleteById(id);
-            return id;
-        } else {
-            throw new PermissionDeniedException("В доступе отказано");
-        }
-    }
-
-    public boolean inspectUserById(Long id) {
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-        UserDetails principal = (UserDetails) authentication.getPrincipal();
-        Optional<UserEntity> user = userRepo.findById(id);
-        return principal.getPassword().equals(user.get().getPassword()) || user.get().getRole().equals("ROLE_ADMIN");
+        todoRepo.deleteById(id);
+        return id;
     }
 }
